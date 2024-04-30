59d76351fda85d9c0795fc3bdbda87bd41512526
KeShih <self.ke.shi@gmail.com>
2024-04-30 16:25:31 +0800

    unfinished

--- kernel/proc.c
+++ kernel/proc.c
@@ -5,6 +5,7 @@
 #include "spinlock.h"
 #include "proc.h"
 #include "defs.h"
+#include <stdlib.h>
 
 struct cpu cpus[NCPU];
 
@@ -196,6 +197,17 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  struct usyscall *u = (struct usyscall *)USYSCALL;
+  u->pid = p->pid;
+
+  if(mappages(pagetable, USYSCALL, PGSIZE,
+              (uint64)u, PTE_R) < 0){
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+
   return pagetable;
 }
 
@@ -206,6 +218,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
